a580ea903631fb28a2401c4de6217f75238cbfd5
surps2020 <60957377+surps2020@users.noreply.github.com>
2020-02-19 11:40:01 +0530

    Update AccountTranscationDaoImpl.java

--- Insto-auto1/src/main/java/com/wholesale/app/dao/AccountTranscationDaoImpl.java
+++ Insto-auto1/src/main/java/com/wholesale/app/dao/AccountTranscationDaoImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
 import com.wholesale.app.model.AccountTranscation;
 
+@Repository("accountTranscationDao")
 public class AccountTranscationDaoImpl implements AccountTranscationDao {
 
 	@PersistenceContext
